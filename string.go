394487656572d374160d3449a2315aaea3a49b03
rivaldojuliano <vlado11jr@gmail.com>
2023-01-20 09:02:05 +0700

    data type string

---
+++ string.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Rivaldo")
+	fmt.Println("Rivaldo Juliano")
+
+	//	function string
+	/*
+	   len("string) = menghitung panjang karakter pada string
+	   "string"[number] = mengambil karakter pada posisi yang ditentukan
+	*/
+
+	fmt.Println(len("Rivaldo"))
+	fmt.Println("Rivaldo Juliano"[0])
+}
